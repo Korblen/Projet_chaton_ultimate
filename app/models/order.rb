28f403451271b7e856bc8039f6353b9c6a603632
Ton nom d'utilisateur github <Ton adresse mail github>
2024-03-12 19:13:24 +0100

    action mailers et mailjet avant verification

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,22 @@ class Order < ApplicationRecord
   # table N-N through
   has_many :order_items
   has_many :items, through: :order_items
+
+
+  after_create :send_alert_to_admin
+  after_create :send_alert_to_user
+
+  private
+
+  def send_alert_to_admin
+    if order.present? 
+      UserMailer.order_alert_admin(admin).deliver_now
+    end
+  end
+
+  def send_alert_to_user
+    if order.present?
+      UserMailer.order_alert_user(user).deliver_now
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   has_many :carts
   
 
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
+  
+
 end
