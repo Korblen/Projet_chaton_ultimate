dffb22b955e7194c024d657084cfa586278d0558
annieherieau <annie.herieau@gmail.com>
2024-03-12 17:32:00 +0100

    ajouter un article

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,21 +1,34 @@
 class CartItemsController < ApplicationController
-  before_action :set_cart_item, only: %i[ destroy ]
+  before_action :set_cart_item, only: %i[ create destroy ]
   
   def create
+    if !@cart_item
+      @cart_item = CartItem.new(
+        cart_id: params[:cart_id], 
+        item_id: params[:item_id]
+      )
+
+      if @cart_item.save
+        redirect_to request.referrer, notice: "L'article a été ajouté à votre panier."
+      else
+        redirect_to request.referrer
+      end
+    end
   end
 
   def destroy
     cart_id = @cart_item.cart_id
     if @cart_item.destroy!
-      redirect_to cart_path(cart_id), notice: "L'article a été supprimé."
+      # redirect_to cart_path(cart_id), notice: "L'article a été supprimé."
+      redirect_to request.referrer, notice: "L'article a été supprimé."
     else
-      render cart_path(cart_id), alert: "Une erreur est survenue: l'article n'a pas été supprimé."
+      redirect_to request.referrer, alert: "Une erreur est survenue: l'article n'a pas été supprimé."
     end
   end
 
   private
   def set_cart_item
-    @cart_item = CartItem.find_by(cart_id: params[:id], item_id: params[:item_id])
+    @cart_item = CartItem.find_by(cart_id: params[:cart_id], item_id: params[:item_id])
   end
 
 end
