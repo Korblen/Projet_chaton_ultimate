ce1d4a6d17273dc5505e2f775e666be280f03823
annieherieau <annie.herieau@gmail.com>
2024-03-13 21:12:59 +0100

    flash

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,5 +5,9 @@ class StaticPagesController < ApplicationController
   def send_contact
     UserMailer.visitor_contact_email(params).deliver_now
     UserMailer.admin_contact_email(params).deliver_now
+    flash[:success] = "Votre message a bien été envoyé."
+    redirect_to :contact
   end
+
+
 end
