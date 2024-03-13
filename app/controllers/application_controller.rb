b1479ad306e3f962c739df1fb77ef9f8144dca3b
Korblen <malo.bastianelli@gmail.com>
2024-03-13 21:29:14 +0100

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :is_alive, :email, :password, :password_confirmation)}
     devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :is_alive, :email, :password, :password_confirmation)}
   end
+  
+  helper_method :admin?
+
+  protected
+
+  def admin?
+    user_signed_in? && current_user.admin
+  end
 
 end
