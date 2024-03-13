d90a40931f0fc554dfe531a97aaca9f3863903c9
annieherieau <annie.herieau@gmail.com>
2024-03-13 19:16:35 +0100

    refactoring form + route

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def contact
   end
+
+  def send_contact
+    ContactMailer.contact(params[:name], params[:email], params[:message]).deliver
+ end
 end
