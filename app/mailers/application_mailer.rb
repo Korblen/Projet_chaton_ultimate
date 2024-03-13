246e645ac73370871b95bdacded3f5f819c570cc
annieherieau <annie.herieau@gmail.com>
2024-03-13 20:47:03 +0100

    default url avec url heroku

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -7,7 +7,7 @@ class ApplicationMailer < ActionMailer::Base
     @email = params[:email]
     @message = params[:message]
     @url = default_url_options
-    mail(to: "annie.herieau@gmail.com", subject: 'Catysfaction: contact')
+    mail(to: "annie.herieau@gmail.com", subject: 'Catysfaction: Nous avons reçu votre message')
   end
 
   def admin_contact_email(params)
@@ -15,7 +15,7 @@ class ApplicationMailer < ActionMailer::Base
     @email = params[:email]
     @message = params[:message]
     @url = default_url_options
-    mail(to: "annie.herieau@gmail.com", subject: 'Catysfaction: contact')
+    mail(to: "annie.herieau@gmail.com", subject: 'Catysfaction: Nouveau contact')
   end
 
 end
