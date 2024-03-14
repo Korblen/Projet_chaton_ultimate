5a60a7481d2eed5b68e2aadc62f395a9eec52bcf
annieherieau <annie.herieau@gmail.com>
2024-03-14 14:17:57 +0100

    seed photo 2/2

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,7 @@ photos=[
     type: "image/jpg"
   },
   {
-    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710368651/cats/daria-averina-_867Jy8LCkI-unsplash_jtr84o.jpg',
+    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710421016/cats/daria-averina-_867Jy8LCkI-unsplash_jtr84o_ax10mj.jpg',
     filename: 'daria-averina-_867Jy8LCkI-unsplash_jtr84o',
     type: "image/jpg"
   },
@@ -87,6 +87,56 @@ photos=[
     url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710420795/cats/Beata-Miro_dkqu4u_oshsox.jpg',
     filename: 'Beata-Miro_dkqu4u',
     type: "image/jpg"
+  },
+  {
+    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710421577/todo/3b74dbc201efeeab0a0ed9f67dc632fc_poasqc_nvcq3c.jpg',
+    filename: '3b74dbc201efeeab0a0ed9f67dc632fc_poasqc_nvcq3c',
+    type: "image/jpg"
+  },
+  {
+    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710421581/todo/5a2a0855f6dde4582e83fd7f23369f79_fnpobo_ojyhbo.jpg',
+    filename: '5a2a0855f6dde4582e83fd7f23369f79_fnpobo_ojyhbo',
+    type: "image/jpg"
+  },
+  {
+    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710421584/todo/632d8c015ba9daaec713ffece50d301e_enofbx_xpbfhu.jpg',
+    filename: '632d8c015ba9daaec713ffece50d301e_enofbx_xpbfhu',
+    type: "image/jpg"
+  },
+  {
+    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710421587/todo/1000_F_486470194_NXXVg9nZnsWicRbKmyMm58BsD9t26nsz_c4hbea_vtuta0.jpg',
+    filename: '1000_F_486470194_NXXVg9nZnsWicRbKmyMm58BsD9t26nsz_c4hbea_vtuta0.',
+    type: "image/jpg"
+  },
+  {
+    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710421591/todo/1000_F_579340210_ZuZPsQPVUkFdEHKKVEX47Q1IxZ9CQnm7_qunp9v_iqhdnr.jpg',
+    filename: '1000_F_579340210_ZuZPsQPVUkFdEHKKVEX47Q1IxZ9CQnm7_qunp9v_iqhdnr',
+    type: "image/jpg"
+  },
+  {
+    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710421595/todo/197500221-funny-cat-in-sunglasses-sits-on-the-roof-of-an-old-house_ksoksf_gt1dto.jpg',
+    filename: '197500221-funny-cat-in-sunglasses-sits-on-the-roof-of-an-old-house_ksoksf_gt1dto',
+    type: "image/jpg"
+  },
+  {
+    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710421598/todo/209403817-a-group-of-cool-cats-wearing-trendy-glasses-scarves-and-hats-striking-a-pose-that-oozes-feline_mkphj4_yolgvf.jpg',
+    filename: '209403817-a-group-of-cool-cats-wearing-trendy-glasses-scarves-and-hats-striking-a-pose-that-oozes-feline_mkphj4_yolgvf',
+    type: "image/jpg"
+  },
+  {
+    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710421602/todo/cute-cat-jumping-transparent-isolated-background-ai_866663-11416_wam58t_jg4d10.avif',
+    filename: 'cute-cat-jumping-transparent-isolated-background-ai_866663-11416_wam58t_jg4d10',
+    type: "image/avif"
+  },
+  {
+    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710421607/todo/ea5fe2108b711accc21d732c191c56f6_ivdo8v_mvdxyh.jpg',
+    filename: 'ea5fe2108b711accc21d732c191c56f6_ivdo8v_mvdxyh',
+    type: "image/jpg"
+  },
+  {
+    url: 'https://res.cloudinary.com/dbu3s1gn0/image/upload/v1710421610/todo/istockphoto-1415563237-612x612_jntxjb_pfizud.jpg',
+    filename: 'istockphoto-1415563237-612x612_jntxjb_pfizud',
+    type: "image/jpg"
   }
 ]
 
@@ -102,39 +152,40 @@ photos.each do |photo|
   )
   i.photo.attach(io: URI.open(photo[:url]), filename: photo[:filename], content_type: photo[:type])
   count += 1
+  puts (count)
 end
-puts( count+ " Items créés")
+puts(" Items créés")
 
 
-# emails_array =['annie.herieau@gmail.com', "r.robena@gmail.com", "malo.bastianelli@gmail.com", "yann.rezigui@gmail.com"]
-# emails_array.each do |e|
-#   user = User.create!(
-#     email: e,
-#     password: "1&Azert",
-#     admin: false
-#   )
-#   chart = user.carts.last
-#   rand(0..3).times do |i|
-#     CartItem.create!(
-#       cart: chart,
-#       item: Item.all.sample
-#     )
-#   end
+emails_array =['annie.herieau@gmail.com', "r.robena@gmail.com", "malo.bastianelli@gmail.com", "yann.rezigui@gmail.com"]
+emails_array.each do |e|
+  user = User.create!(
+    email: e,
+    password: "1&Azert",
+    admin: [true, false].sample
+  )
+  chart = user.carts.last
+  rand(0..3).times do |i|
+    CartItem.create!(
+      cart: chart,
+      item: Item.all.sample
+    )
+  end
 
-#   rand(1..4).times do |i|
-#     order = Order.create!(
-#       user: user
-#     )
-#     rand(1..6).times do |i|
-#       OrderItem.create!(
-#         order: order,
-#         item: Item.all.sample
-#       )
-#     end
-#   end
+  rand(1..4).times do |i|
+    order = Order.create!(
+      user: user
+    )
+    rand(1..6).times do |i|
+      OrderItem.create!(
+        order: order,
+        item: Item.all.sample
+      )
+    end
+  end
   
-# end
-# puts("> 4 Users créés")
+end
+puts("> 4 Users créés")
 
 
 
